2902a327d9a846a76a769e0a5dc6bc68af80628c
tzvi1000000 <tzvi1000000@gmail.com>
2024-05-29 23:26:10 +0300

    color

--- main.rs
+++ main.rs
@@ -6,31 +6,68 @@ enum CellState {
     Empty,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct Cell(CellState, Color);
+
 fn window_conf() -> Conf {
     Conf {
         window_title: "sand".to_owned(),
-        window_width: 1000,
-        window_height: 1000,
+        window_width: 1500,
+        window_height: 1500,
+        // fullscreen: true,
         ..Default::default()
     }
 }
 
 #[macroquad::main(window_conf)]
 async fn main() {
+    const BACKGROUND: Color = WHITE;
     let w = screen_width() as usize;
     let h = screen_height() as usize;
-    let brush_size: i32 = 10;
+    let mut brush_size: i32 = 20;
+    let mut color_index = 1;
 
-    let mut cells = vec![CellState::Empty; w * h];
-    let mut buffer = vec![CellState::Empty; w * h];
+    let mut cells = vec![Cell(CellState::Empty, BACKGROUND); w * h];
+    let mut buffer = vec![Cell(CellState::Empty, BACKGROUND); w * h];
+    let mut image = Image::gen_image_color(w as u16, h as u16, BACKGROUND);
 
-    let mut image = Image::gen_image_color(w as u16, h as u16, WHITE);
+    let color_palette = [
+        BEIGE, BLACK, BLANK, BLUE, BROWN, DARKBLUE, DARKBROWN, DARKGRAY, DARKGREEN, DARKPURPLE,
+        GOLD, GRAY, GREEN, LIGHTGRAY, LIME, MAGENTA, MAROON, ORANGE, PINK, PURPLE, RED, SKYBLUE,
+        VIOLET, YELLOW,
+    ];
+    let color_palette_names = [
+        "BEIGE",
+        "BLACK",
+        "BLANK",
+        "BLUE",
+        "BROWN",
+        "DARKBLUE",
+        "DARKBROWN",
+        "DARKGRAY",
+        "DARKGREEN",
+        "DARKPURPLE",
+        "GOLD",
+        "GRAY",
+        "GREEN",
+        "LIGHTGRAY",
+        "LIME",
+        "MAGENTA",
+        "MAROON",
+        "ORANGE",
+        "PINK",
+        "PURPLE",
+        "RED",
+        "SKYBLUE",
+        "VIOLET",
+        "YELLOW",
+    ];
 
-    for cell in cells.iter_mut() {
-        if rand::gen_range(0, 30) == 0 {
-            *cell = CellState::Full;
-        }
-    }
+    // for cell in cells.iter_mut() {
+    // if rand::gen_range(0, 30) == 0 {
+    // *cell = Cell(CellState::Full, BLUE);
+    // }
+    // }
     let texture = Texture2D::from_image(&image);
 
     loop {
@@ -42,23 +79,92 @@ async fn main() {
         for y in 0..h as i32 {
             for x in 0..w as i32 {
                 let current_cell = cells[y as usize * w + x as usize];
-                if current_cell == CellState::Empty {
+                if current_cell.0 == CellState::Empty {
                     continue;
                 }
-                if y + 1 >= h as i32 {
+                if y + 2 >= h as i32 {
                     continue;
                 }
                 let bottom_cell = cells[(y + 1) as usize * w + x as usize];
-                if bottom_cell == CellState::Empty {
-                    buffer[y as usize * w + x as usize] = CellState::Empty;
-                    buffer[(y + 1) as usize * w + x as usize] = CellState::Full;
+                if bottom_cell.0 == CellState::Empty {
+                    buffer[y as usize * w + x as usize].0 = CellState::Empty;
+                    buffer[(y + 1) as usize * w + x as usize].0 = CellState::Full;
+                    buffer[(y + 1) as usize * w + x as usize].1 =
+                        buffer[y as usize * w + x as usize].1;
+                    buffer[y as usize * w + x as usize].1 = BACKGROUND;
+                } else if x - 1 >= 0
+                    && cells[(y + 1) as usize * w + (x - 1) as usize].0 == CellState::Empty
+                {
+                    buffer[y as usize * w + x as usize].0 = CellState::Empty;
+                    buffer[(y + 1) as usize * w + (x - 1) as usize].0 = CellState::Full;
+                    buffer[(y + 1) as usize * w + (x - 1) as usize].1 =
+                        buffer[y as usize * w + x as usize].1;
+                    buffer[y as usize * w + x as usize].1 = BACKGROUND;
+                } else if x + 1 < w as i32
+                    && cells[(y + 1) as usize * w + (x + 1) as usize].0 == CellState::Empty
+                {
+                    buffer[y as usize * w + x as usize].0 = CellState::Empty;
+                    buffer[(y + 1) as usize * w + (x + 1) as usize].0 = CellState::Full;
+                    buffer[(y + 1) as usize * w + (x + 1) as usize].1 =
+                        buffer[y as usize * w + x as usize].1;
+                    buffer[y as usize * w + x as usize].1 = BACKGROUND;
                 }
             }
         }
 
-        let mouse_x: f32;
-        let mouse_y: f32;
+        let mut mouse_x: f32;
+        let mut mouse_y: f32;
+
+        if is_key_pressed(KeyCode::Right) {
+            color_index += 1;
+            println!("{}", color_palette_names[color_index]);
+            if color_index == color_palette.len() - 1 {
+                color_index = 0;
+            }
+        }
+        if is_key_pressed(KeyCode::Left) {
+            color_index -= 1;
+            println!("{}", color_palette_names[color_index]);
+            if color_index == 0 {
+                color_index = color_palette.len() - 1;
+            }
+        }
+        if is_key_pressed(KeyCode::Up) {
+            brush_size += 2;
+            println!("{}", brush_size);
+        }
+        if is_key_pressed(KeyCode::Down) {
+            brush_size -= 2;
+            println!("{}", brush_size);
+        }
+
         if is_mouse_button_down(MouseButton::Left) {
+            (mouse_x, mouse_y) = mouse_position();
+            for j in -brush_size..=brush_size {
+                for i in -brush_size..=brush_size {
+                    // out of bounds
+                    if ((i * i + j * j) as f32).sqrt() >= brush_size as f32 {
+                        continue;
+                    }
+                    if mouse_y + (j as f32) < 0.0
+                        || mouse_y + (j as f32) >= h as f32
+                        || mouse_x + (i as f32) < 0.0
+                        || mouse_x + (i as f32) >= w as f32
+                        || rand::gen_range(0, 2) == 0
+                    {
+                        continue;
+                    } else {
+                        buffer[(mouse_y + (j as f32)) as usize * w
+                            + (mouse_x + (i as f32)) as usize]
+                            .0 = CellState::Full;
+                        buffer[(mouse_y + (j as f32)) as usize * w
+                            + (mouse_x + (i as f32)) as usize]
+                            .1 = color_palette[color_index];
+                    }
+                }
+            }
+        }
+        if is_mouse_button_down(MouseButton::Right) {
             (mouse_x, mouse_y) = mouse_position();
             for j in -brush_size..=brush_size {
                 for i in -brush_size..=brush_size {
@@ -74,24 +180,19 @@ async fn main() {
                         continue;
                     } else {
                         buffer[(mouse_y + (j as f32)) as usize * w
-                            + (mouse_x + (i as f32)) as usize] = CellState::Full;
+                            + (mouse_x + (i as f32)) as usize]
+                            .0 = CellState::Empty;
+                        buffer[(mouse_y + (j as f32)) as usize * w
+                            + (mouse_x + (i as f32)) as usize]
+                            .1 = BACKGROUND;
                     }
                 }
             }
         }
-        // if is_mouse_button_down(MouseButton::Right) {}
 
         for i in 0..buffer.len() {
             cells[i] = buffer[i];
-
-            image.set_pixel(
-                (i % w) as u32,
-                (i / w) as u32,
-                match buffer[i as usize] {
-                    CellState::Full => GRAY,
-                    CellState::Empty => WHITE,
-                },
-            );
+            image.set_pixel((i % w) as u32, (i / w) as u32, buffer[i as usize].1);
         }
 
         texture.update(&image);
@@ -101,96 +202,4 @@ async fn main() {
         next_frame().await
     }
 }
-
-// #[derive(Copy, Clone, Debug, PartialEq, Eq)]
-// enum CellState {
-//     Alive,
-//     Dead,
-// }
-
-// #[macroquad::main("Life")]
-// async fn main() {
-//     request_new_screen_size(500.0,500.0);
-//     let w = screen_width() as usize;
-//     let h = screen_height() as usize;
-
-//     let mut cells = vec![CellState::Dead; w * h];
-//     let mut buffer = vec![CellState::Dead; w * h];
-
-//     let mut image = Image::gen_image_color(w as u16, h as u16, WHITE);
-
-//     for cell in cells.iter_mut() {
-//         if rand::gen_range(0, 5) == 0 {
-//             *cell = CellState::Alive;
-//         }
-//     }
-//     let texture = Texture2D::from_image(&image);
-
-//     loop {
-//         clear_background(WHITE);
-
-//         let w = image.width();
-//         let h = image.height();
-
-//         for y in 0..h as i32 {
-//             for x in 0..w as i32 {
-//                 let mut neighbors_count = 0;
-
-//                 for j in -1i32..=1 {
-//                     for i in -1i32..=1 {
-//                         // out of bounds
-//                         if y + j < 0 || y + j >= h as i32 || x + i < 0 || x + i >= w as i32 {
-//                             continue;
-//                         }
-//                         // cell itself
-//                         if i == 0 && j == 0 {
-//                             continue;
-//                         }
-
-//                         let neighbor = cells[(y + j) as usize * w + (x + i) as usize];
-//                         if neighbor == CellState::Alive {
-//                             neighbors_count += 1;
-//                         }
-//                     }
-//                 }
-
-//                 let current_cell = cells[y as usize * w + x as usize];
-//                 buffer[y as usize * w + x as usize] = match (current_cell, neighbors_count) {
-//                     // Rule 1: Any live cell with fewer than two live neighbours
-//                     // dies, as if caused by underpopulation.
-//                     (CellState::Alive, x) if x < 2 => CellState::Dead,
-//                     // Rule 2: Any live cell with two or three live neighbours
-//                     // lives on to the next generation.
-//                     (CellState::Alive, 2) | (CellState::Alive, 3) => CellState::Alive,
-//                     // Rule 3: Any live cell with more than three live
-//                     // neighbours dies, as if by overpopulation.
-//                     (CellState::Alive, x) if x > 3 => CellState::Dead,
-//                     // Rule 4: Any dead cell with exactly three live neighbours
-//                     // becomes a live cell, as if by reproduction.
-//                     (CellState::Dead, 3) => CellState::Alive,
-//                     // All other cells remain in the same state.
-//                     (otherwise, _) => otherwise,
-//                 };
-//             }
-//         }
-
-//         for i in 0..buffer.len() {
-//             cells[i] = buffer[i];
-
-//             image.set_pixel(
-//                 (i % w) as u32,
-//                 (i / w) as u32,
-//                 match buffer[i as usize] {
-//                     CellState::Alive => BLACK,
-//                     CellState::Dead => WHITE,
-//                 },
-//             );
-//         }
-
-//         texture.update(&image);
-
-//         draw_texture(&texture, 0., 0., WHITE);
-
-//         next_frame().await
-//     }
-// }
+//pink == Color::new(1.0, 0.4, 0.7, 1.0);
