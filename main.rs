a1730e000c6f22cf6c078bcc10c0e99fe917a2e7
tzvi1000000 <tzvi1000000@gmail.com>
2024-06-02 18:11:19 +0300

    optimisation

--- main.rs
+++ main.rs
@@ -21,12 +21,13 @@ fn window_conf() -> Conf {
 
 #[macroquad::main(window_conf)]
 async fn main() {
-    const BACKGROUND: Color = BLACK;
+    const BACKGROUND: Color = WHITE;
     let w = screen_width() as usize;
     let h = screen_height() as usize;
     let mut brush_size: i32 = 40;
     let mut color_index = 10;
-    let mut optimisation_line: usize = 30;
+    let mut optimisation_line: usize = h;
+    let mut help_message_time: i32 = 200;
 
     let mut cells = vec![Cell(CellState::Empty, BACKGROUND); w * h];
     let mut buffer = vec![Cell(CellState::Empty, BACKGROUND); w * h];
@@ -50,7 +51,26 @@ async fn main() {
         let h = image.height();
 
         (mouse_x, mouse_y) = mouse_position();
-        for y in 0..h as i32 {
+
+        //find the first line that have an empty cell
+        let mut full_line = true;
+        for y in (0..(optimisation_line - 1) as i32).rev() {
+            for x in 0..w as i32 {
+                if cells[y as usize * w + x as usize].0 == CellState::Empty {
+                    full_line = false;
+                    break;
+                }
+            }
+            if full_line {
+                if optimisation_line > 0 {
+                    optimisation_line -= 1;
+                }
+            } else {
+                break;
+            }
+        }
+
+        for y in 0..optimisation_line as i32 {
             for x in 0..w as i32 {
                 let current_cell = cells[y as usize * w + x as usize];
                 if current_cell.0 == CellState::Empty {
@@ -85,7 +105,7 @@ async fn main() {
                 }
             }
         }
-
+        //keyboard input
         if is_key_pressed(KeyCode::Right) {
             color_index += 1;
             if color_index == color_palette.len() - 1 {
@@ -179,6 +199,8 @@ async fn main() {
                     }
                 }
             }
+            //reset the optimisation line
+            optimisation_line = h;
         }
 
         for i in 0..buffer.len() {
@@ -188,6 +210,8 @@ async fn main() {
 
         texture.update(&image);
         draw_texture(&texture, 0., 0., WHITE);
+
+        //UI
         draw_circle(w as f32 - 30.0, 30.0, 15.0, color_palette[color_index]);
         draw_circle_lines(
             mouse_x,
@@ -196,17 +220,43 @@ async fn main() {
             2.0,
             color_palette[color_index],
         );
+        if help_message_time > 0 {
+            draw_text(
+                "left mouse buttom to add sand",
+                w as f32 / 2.0 - 200.0,
+                120.0,
+                30.0,
+                DARKGRAY,
+            );
+            draw_text(
+                "right mouse buttom to remove sand",
+                w as f32 / 2.0 - 220.0,
+                140.0,
+                30.0,
+                DARKGRAY,
+            );
+            draw_text(
+                "roll mouse wheel to change brush size",
+                w as f32 / 2.0 - 250.0,
+                180.0,
+                30.0,
+                DARKGRAY,
+            );
+        }
+        if help_message_time > -50 {
+            draw_text(
+                "press and roll mouse wheel to change color",
+                w as f32 / 2.0 - 270.0,
+                200.0,
+                30.0,
+                DARKGRAY,
+            );
+
+            help_message_time -= 1;
+        }
         //dbg
-        draw_line(
-            0.0,
-            optimisation_line as f32,
-            w as f32,
-            optimisation_line as f32,
-            2.0,
-            RED,
-        );
+        // draw_line(0.0,optimisation_line as f32,w as f32,optimisation_line as f32,2.0,RED,);
 
         next_frame().await
     }
 }
-//pink == Color::new(1.0, 0.4, 0.7, 1.0);
